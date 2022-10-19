964cc6d8ca0635370d769a45fecb959fac16c9b9
Luis Muñoz <lmunozm1702@gmail.com>
2022-10-19 18:30:39 -0300

    fix linter errors

--- assets/javascript/static-data-program.js
+++ assets/javascript/static-data-program.js
@@ -1,32 +1,33 @@
 const mainProgram = {
-    program1: {
-        "icon": "assets/images/program_icon_01.png",
-        "title": "Research",
-        "description": "Medical and Health Sciences Research",
-        "alt": "Medical and Health Icon"
-    },
-    program2: {
-        "icon": "assets/images/program_icon_02.png",
-        "title": "Lecture",
-        "description": "Digital program consists of an e-book of relevant studies to download for future reading on November 11.",
-        "alt": "Lecture Icon"
-    },
-    program3: {
-        "icon": "assets/images/program_icon_03.png",
-        "title": "Research",
-        "description": "Human and Social Sciences Research.",
-        "alt": "Human and Social Icon"
-    },
-    program4: {
-        "icon": "assets/images/program_icon_04.png",
-        "title": "Virtual Meet",
-        "description": "The digital presentation session will start at 12 p.m. (midday) on November 10 London local time.",
-        "alt": "Virtual Meet Icon"
-    }, program5: {
-        "icon": "assets/images/program_icon_05.png",
-        "title": "Research",
-        "description": "Engineering and Physical Sciences Research.",
-        "alt": "Engineering Icon"
-    }
-}
+  program1: {
+    icon: 'assets/images/program_icon_01.png',
+    title: 'Research',
+    description: 'Medical and Health Sciences Research',
+    alt: 'Medical and Health Icon',
+  },
+  program2: {
+    icon: 'assets/images/program_icon_02.png',
+    title: 'Lecture',
+    description: 'Digital program consists of an e-book of relevant studies to download for future reading on November 11.',
+    alt: 'Lecture Icon',
+  },
+  program3: {
+    icon: 'assets/images/program_icon_03.png',
+    title: 'Research',
+    description: 'Human and Social Sciences Research.',
+    alt: 'Human and Social Icon',
+  },
+  program4: {
+    icon: 'assets/images/program_icon_04.png',
+    title: 'Virtual Meet',
+    description: 'The digital presentation session will start at 12 p.m. (midday) on November 10 London local time.',
+    alt: 'Virtual Meet Icon',
+  },
+  program5: {
+    icon: 'assets/images/program_icon_05.png',
+    title: 'Research',
+    description: 'Engineering and Physical Sciences Research.',
+    alt: 'Engineering Icon',
+  },
+};
 export default mainProgram;
