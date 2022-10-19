1f6b485589c85bafb5867b72eb4f71c0c62490d3
Luis Muñoz <lmunozm1702@gmail.com>
2022-10-19 17:47:21 -0300

    Add about page for both devices
    Update:
    - index.html, styles.css, main.js to include about page for both devices.
    - static-data*.js to update the data for speakers and program.
    
    Add images for speakers and icons for program into /assets/images folder.
    
    Add about.html page.

--- assets/javascript/static-data-speakers.js
+++ assets/javascript/static-data-speakers.js
@@ -1,59 +1,45 @@
 const speakers = {
     speaker1: {
-        "name": "Yochai Benkler",
+        "name": "Akash Bera",
         "image": "assets/images/speaker_01.png",
-        "alt": "",
-        "role": "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-        "comment": "Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006."
+        "alt": "Akash Bera",
+        "role": "University of Mumbai, India",
+        "comment": "Mechanistic Structural Insights into the UV Induced Apoptosis via Bcl-2 proteins."
     },
     speaker2: {
-        "name": "Yochai Benkler",
-        "image": "assets/images/speaker_01.png",
-        "alt": "",
-        "role": "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-        "comment": "Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006."
+        "name": "Jose A. Foppiani",
+        "image": "assets/images/speaker_02.png",
+        "alt": "Jose A. Foppiani",
+        "role": "First Faculty of Medicine, Charles University, Czechia",
+        "comment": "Indications and Results of the Use of Facial Fat Grafting and Fillers in Complications of Cosmetic Facial Augmentation"
     },
     speaker3: {
-        "name": "Yochai Benkler",
-        "image": "assets/images/speaker_01.png",
-        "alt": "",
-        "role": "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-        "comment": "Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006."
+        "name": "Malihe Moeinin",
+        "image": "assets/images/speaker_03.png",
+        "alt": "Malihe Moeinin",
+        "role": "Kings College hospital, United Kingdom",
+        "comment": "Characterization of White Spot Lesion Using Focused Ion Beam - Scanning Electron Microscopy"
     },
     speaker4: {
         "name": "Yochai Benkler",
-        "image": "assets/images/speaker_01.png",
-        "alt": "",
-        "role": "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-        "comment": "Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006."
+        "image": "assets/images/speaker_04.png",
+        "alt": "Yochai Benkler",
+        "role": "Ramakrishna Mission Vivekananda Educational and Research Institute",
+        "comment": "Utility of Range of Motion Measurements on Classification of Athletes"
     },
     speaker5: {
-        "name": "Yochai Benkler",
-        "image": "assets/images/speaker_01.png",
-        "alt": "",
-        "role": "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-        "comment": "Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006."
+        "name": "Arumay Jana",
+        "image": "assets/images/speaker_05.png",
+        "alt": "Arumay Jana",
+        "role": "Jadavpur University",
+        "comment": "A Comparative Study on Achievement Motivation and Sports Competition Anxiety among the Students of Different Tier of Academic Hierarchy"
     },
     speaker6: {
-        "name": "Yochai Benkler",
-        "image": "assets/images/speaker_01.png",
-        "alt": "",
-        "role": "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-        "comment": "Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006."
-    },
-    speaker7: {
-        "name": "Yochai Benkler",
-        "image": "assets/images/speaker_01.png",
-        "alt": "",
-        "role": "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-        "comment": "Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006."
-    },
-    speaker8: {
-        "name": "Yochai Benkler",
-        "image": "assets/images/speaker_01.png",
-        "alt": "",
-        "role": "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-        "comment": "Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006."
+        "name": "Jamie L. Stangel",
+        "image": "assets/images/speaker_06.png",
+        "alt": "Jamie L. Stangel",
+        "role": "Assistant Professor of Kinesiology, Director of Sport Management Program",
+        "comment": "Measuring the Effect of Intercollegiate Athletic Success on Private Giving and Enrollment"
     }
 }
 export default speakers;
